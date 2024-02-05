a6f6c0e6b5040f453499dc626a1c59d1cec03b6f
Joseph Mi <jmjosephmi@gmail.com>
2024-02-05 17:40:05 -0500

    q1-4

---
+++ online2.c
@@ -0,0 +1,69 @@
+// Solution for Q_1
+int a = 42;
+
+// Solution for Q_2
+int *pa = &a;
+
+// Solution for Q_3
+*pa = 43;
+
+// Solution for Q_4
+int b = 90;
+pa = &b;
+
+// Solution for Q_5
+
+// Solution for Q_6
+
+
+// Solution for Q_7
+
+// Solution for Q_8
+
+// Solution for Q_9
+
+// Solution for Q_10
+
+// Solution for Q_11
+
+// Solution for Q_12
+
+// Solution for Q_13
+
+// Solution for Q_14
+
+// Solution for Q_15
+
+// Solution for Q_16
+
+// Solution for Q_17
+
+// Solution for Q_18
+
+// Solution for Q_19
+
+// Solution for Q_20
+
+// Solution for Q_21
+
+// Solution for Q_22
+
+// Solution for Q_23
+
+// Solution for Q_24
+
+// Solution for Q_25
+
+// Solution for Q_26
+
+// Solution for Q_27
+
+// Solution for Q_28
+
+// Solution for Q_29
+
+// Solution for Q_30
+
+// Solution for Q_31
+
+// Solution for Q_32
